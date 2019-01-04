b6e24eaf8b446eaa4ce805876995b9995bc739a6
Jared McMahon <jared.mcmahon@tooeleschools.org>
2019-01-04 14:11:04 -0700

    Adding more stuff for term 2 final

--- Python-2nd term/Term 2 Final/Term 2 Final.py
+++ Python-2nd term/Term 2 Final/Term 2 Final.py
@@ -7,7 +7,7 @@ import sys
 def open_file(file_name,mode):
     """Open File."""
     try:
-        file=open(file_name,mode)
+        file = open(file_name,mode)
     except IOError as e:
         print("unable to open the file", file_name, "Ending program.\n", e)
         input("\n\nPress the enter key to exit.")
@@ -26,9 +26,51 @@ def next_line(the_file):
         line = line.replace("/","\n")
         return line
 
-file = open_file("test_file.txt","w")
-file.write("this/is a/test")
-file.close()
-file = open_file("test_file.txt","r")
-line = next_line(file)
-print(line)
+def next_block(the_file):
+    category = next_line(the_file)
+    question = next_line(the_file)
+    answers = []
+    for i in range(4):
+        temp_answer = next_line(the_file)
+        answers.append(temp_answer)
+        
+    correct = next_line(the_file)
+    
+    if correct:
+        correct = correct[0]
+    
+    explenation = next_line(the_file)
+    
+    return category, question, answers, correct, explenation
+
+def welcome(title):
+    """Welcoming the player."""
+    print("\t\tWelcome to my Trivia Quiz!")
+    print("\t\t", title, "\n")
+
+
+def main():
+    the_file = open_file("trivia_stuff.txt","r")
+    title = next_line(the_file)
+    welcome(title)
+    score = 0
+    next_block(the_file)
+    while category:
+        print(category)
+        print(question)
+        for i in range(4):
+            print(answers[i-1])
+        guesss = input("What do you think the answer is?: ")
+        if guess == answer:
+            print("Yay! you got it right!")
+            score = score + 1
+        else:
+            print("That was wrong. :[")
+        print(explenation)
+        print("Your score is now", score)
+        next_block(the_file)
+    trivia_stuff.txt.close()
+    print("Thanks for playing my Trivia quiz.\n\t\tYor Score was ", score, "out of 10")
+    input("Press the enter key to exit")
+
+main()
